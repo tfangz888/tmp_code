a77d42a48ec97a2673dbc3aefcb6368417531f98
Tfangz <tfangz@ymail.com>
2020-11-10 17:05:28 +0800

    Update 高精度时间测量.cpp

--- 高精度时间测量.cpp
+++ 高精度时间测量.cpp
@@ -3,6 +3,13 @@
 #include <thread>
 using namespace std;
 
+uint64_t rdtsc()
+{
+   uint32_t hi, lo;
+   __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+   return ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+}
+
 // A sample function whose time taken to
 // be measured
 void fun()
@@ -19,8 +26,11 @@ int main()
 
     // unsync the I/O of C and C++
     ios_base::sync_with_stdio(false);
+    uint64_t t0 = rdtsc();
 
     fun();
+
+    uint64_t t1 = rdtsc();
 
     auto end = chrono::high_resolution_clock::now();
 
@@ -33,5 +43,7 @@ int main()
     cout << "time taken by program is : " << fixed
         << time_taken << setprecision(9);
     cout << " sec" << endl;
+    cout << " rdtsc = " << unsigned(t1-t0) << endl;
+
     return 0;
 }
