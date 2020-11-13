acaf5509320c322ed12d9ba1f26ac087964a495a
Tfangz <tfangz@ymail.com>
2020-11-13 16:30:40 +0800

    Update 南华期货价格指数.py

--- 南华期货价格指数.py
+++ 南华期货价格指数.py
@@ -1,7 +1,7 @@
 # PM 普麦, 取数据出错,去掉
 # 去掉中国金融期货交易所: IF , TF, T, "IC", "IH", 
 # 去掉上海国际能源交易中心: "SC", "NR", 
-syms = ["A", "AL", "CU", "RU", "M", "CF", "FU", "C", "SR", "Y", "TA", "ZN", "L", "P", "AU", "RB", "WR", "V", "PB", "J", "AG", "RO", "ER", "WS", "FG", "RS", "RM", "JM", "BU", "I", "JD", "JR", "BB", "FB", "PP", "HC", "LR", "SM", "SF", "CS", "SN", "NI", "ME", "TC", "CY", "CY", "AP", "SC", "SP", "EG", "CJ", "UR", "RR", "B", "SS", "EB", "SA"]
+syms = ["A", "AL", "CU", "RU", "M", "CF", "FU", "C", "SR", "Y", "TA", "ZN", "L", "P", "AU", "RB", "WR", "V", "PB", "J", "AG", "RO", "ER", "WS", "FG", "RS", "RM", "JM", "BU", "I", "JD", "JR", "BB", "FB", "PP", "HC", "LR", "SM", "SF", "CS", "SN", "NI", "ME", "TC", "CY", "AP", "SC", "SP", "EG", "CJ", "UR", "RR", "B", "SS", "EB", "SA"]
 
 import akshare as ak
 import pandas as pd
